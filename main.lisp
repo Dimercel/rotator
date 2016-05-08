77130898ec191dd8a217961e7936d9c36651286b
Dimercel <xolcman@gmail.com>
2016-05-08 11:10:58 +0300

    File API

--- main.lisp
+++ main.lisp
@@ -1,8 +1,26 @@
 (defpackage :rotator
-  (:export :test)
+  (:export :test
+           :config-dir
+           :rules-config-exist?
+           :rules-config-path)
   (:use :common-lisp :cxml))
 
 (in-package :rotator)
 
 (defun test ()
   "test")
+
+(defun config-dir ()
+  (pathname
+   (concatenate 'string
+                (directory-namestring (user-homedir-pathname))
+                ".rotator/")))
+
+(defun rules-config-path ()
+  (merge-pathnames (config-dir) #p"config.xml"))
+
+(defun rules-config-exist? ()
+  (let ((exist (probe-file (rules-config-path))))
+    (if exist
+        T
+        nil)))
