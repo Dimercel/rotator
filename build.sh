9b6f852347f5ca86b6f770e9b93415b843254485
Dimercel <xolcman@gmail.com>
2016-05-09 16:47:50 +0300

    Build file

---
+++ build.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/sh
+
+sbcl --no-userinit --no-sysinit --non-interactive \
+     --load ~/quicklisp/setup.lisp \
+     --eval '(ql:quickload "rotator")' \
+     --eval '(ql:write-asdf-manifest-file "rotator-manifest.txt")'
+
+buildapp --manifest-file rotator-manifest.txt --load-system rotator \
+         --output rotator-exe \
+         --entry rotator:main
