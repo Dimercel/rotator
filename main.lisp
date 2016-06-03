40fe1509cea3643da83bc2fe9e16b0bf2268bd32
Dimercel <xolcman@gmail.com>
2016-06-03 19:14:21 +0300

    More beautiful main-loop

--- main.lisp
+++ main.lisp
@@ -1,10 +1,6 @@
 (defpackage :rotator
   (:export :main
-           :log-file-path
-           :bind-code
-           :check-condition
-           :main-loop
-           :init-logger)
+           :main-loop)
   (:use :common-lisp :cl-fad)
   (:import-from :cl-log
                 :log-manager
@@ -16,6 +12,8 @@
                 :file-size-more
                 :file-name-match
                 :file-size-less)
+  (:import-from :rutils
+                :with-gensyms)
   (:import-from :rotator.config
                 :config-dir-path
                 :rules-config-exists?
@@ -41,6 +39,19 @@
     ("file-size-more"  (file-size-more path limit))
     ("file-size-less"  (file-size-less path limit))))
 
+(defun is-file? (path)
+  (if (not (cl-fad:directory-pathname-p path))
+      t
+      nil))
+
+(defmacro with-item-in-dir (symbol dir-path predicate &body body)
+  (rutils:with-gensyms (dir-item)
+    `(if (cl-fad:directory-exists-p ,dir-path)
+         (dolist (,dir-item (cl-fad:list-directory ,dir-path))
+           (if (,predicate ,dir-item)
+               (let ((,symbol ,dir-item))
+                 ,@body))))))
+
 (defun log-file-path ()
   "Возвращает путь до главного лог-файла"
   (merge-pathnames (config-dir-path) #p"rotator.log"))
@@ -65,15 +76,14 @@
 (defun main-loop ()
   (let ((directories (parse)))
     (dolist (dir directories)
-      (dolist (file (cl-fad:list-directory (gethash :path dir)))
-        (let ((conditions (gethash :conditions dir)))
-          (if (not (cl-fad:directory-pathname-p file))
-              (if (all-conditions-true? file conditions)
-                  (format t "Rotate ~S~%" (namestring file)))))))))
+      (let ((conditions (gethash :conditions dir)))
+        (with-item-in-dir file (gethash :path dir) is-file?
+          (if (all-conditions-true? file conditions)
+              (format t "Rotate ~S~%" (namestring file))))))))
 
 (defun main (argv)
   (declare (ignore argv))
   (cond ((not (rules-config-exists?))
          (format t "Файл ~S не существует!"
                  (namestring (rules-config-path))))
-        (t (print "It's work!"))))
+        (t (main-loop))))
